d88a8f8655c1b9af304dc46f10351d8c18acd239
Abdelrahman Elrefai <team.man0@yahoo.com>
2019-12-11 14:13:31 +0200

    Added Debug & Html Plugins

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/main', function (){
+    return view('layouts.web_app');
+});
